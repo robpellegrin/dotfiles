a68f3ae6554b4b399f326609a7c4438089795534
Rob Pellegrin <>
2024-11-05 17:55:45 -0500

    Added lualine plugin

---
+++ .config/nvim/lua/plugins/lualine.lua
@@ -0,0 +1,20 @@
+return {
+  'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+  config = function()
+    require('lualine').setup({
+      options = {
+        theme = 'auto',
+      },
+      sections = {
+        lualine_b = {'branch', 'diff'},
+        lualine_c = {'filename', 'diagnostics'},
+        lualine_x = {'filetype'},
+        lualine_y = {'progress'},
+        lualine_z = {'location'}
+      },
+      extensions = {'fugitive'}
+    })
+  end
+}
+
