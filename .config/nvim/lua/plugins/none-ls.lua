168cd757877e424e1f38623734d0835f81c6d793
Rob Pellegrin <>
2024-11-06 10:34:23 -0500

    Added clang-format as formatter

--- .config/nvim/lua/plugins/none-ls.lua
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -1,15 +1,16 @@
 -- none-ls.lua
 return {
-  "nvimtools/none-ls.nvim",
-  config = function()
-    local null_ls = require("null-ls")
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-      },
-    })
+	"nvimtools/none-ls.nvim",
+	config = function()
+		local null_ls = require("null-ls")
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.clang_format,
+			},
+		})
 
-    -- Keybinding for formatting.
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		-- Keybinding for formatting.
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+	end,
 }
