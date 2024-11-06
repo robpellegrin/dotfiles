89fe5478516d1ddafa86d036d211dec3a874be9d
Rob Pellegrin <>
2024-11-06 08:54:33 -0500

    New plugin for none-ls

---
+++ .config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,15 @@
+-- none-ls.lua
+return {
+  "nvimtools/none-ls.nvim",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+
+    -- Keybinding for formatting.
+    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+  end,
+}
