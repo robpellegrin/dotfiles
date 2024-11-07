a8577c7a80b95ddb2e157565dfae2f9e005208f1
Rob Pellegrin <>
2024-11-07 15:40:40 -0500

    Various changes related to lazy.vim update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,17 +2,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -29,8 +29,7 @@ vim.api.nvim_set_keymap("i", "<C-x>", "<Esc>:q<CR>", { noremap = true, silent = 
 vim.api.nvim_set_keymap("n", "<C-x>", ":q<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-X>", ":qa!<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', 'D', 'dd', { noremap = true, silent = true })
-
+vim.api.nvim_set_keymap("n", "D", "dd", { noremap = true, silent = true })
 
 -- Enable line numbers
 vim.cmd("set number")
@@ -43,3 +42,7 @@ vim.cmd("set shiftwidth=2")
 
 -- Remove '~' on empty lines
 vim.opt.fillchars = "eob: " -- Set the 'eob' (end-of-buffer) fill character to a space
+
+-- Cursor blink
+vim.opt.guicursor = "a:blinkon1"
+
