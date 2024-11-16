19ef39d81dbda15b0058377775fee09908db6689
Rob Pellegrin <>
2024-11-15 23:26:41 -0500

    Enabled spell checking

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,21 +2,21 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-	if vim.v.shell_error ~= 0 then
-		vim.api.nvim_echo({
-			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-			{ out, "WarningMsg" },
-			{ "\nPress any key to exit..." },
-		}, true, {})
-		vim.fn.getchar()
-		os.exit(1)
-	end
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out,                            "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Set mapleader before lazy.nvim is loaded
+-- Set mapleader before lazy.nvim is loaded chickn
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
@@ -31,8 +31,7 @@ vim.api.nvim_set_keymap("n", "<C-X>", ":qa!<CR>", { noremap = true, silent = tru
 
 vim.api.nvim_set_keymap("n", "D", "dd", { noremap = true, silent = true })
 
-vim.api.nvim_set_keymap('n', '<leader>t', ':FloatermToggle<CR>', { noremap = true })
-
+vim.api.nvim_set_keymap("n", "<leader>t", ":FloatermToggle<CR>", { noremap = true })
 
 -- Enable line numbers
 vim.cmd("set number")
@@ -48,3 +47,9 @@ vim.opt.fillchars = "eob: " -- Set the 'eob' (end-of-buffer) fill character to a
 
 -- Cursor blink
 vim.opt.guicursor = "a:blinkon1"
+
+-- Enable spell checking
+vim.opt.spell = true
+
+-- Set languages for spell checking
+vim.opt.spelllang = { "en_us" }
