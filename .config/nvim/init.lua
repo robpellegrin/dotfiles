82648ca00ced547c8cec9a5f5595a95e0317d83d
Rob Pellegrin <>
2024-11-18 20:55:13 -0500

    Moved keybindings into separate file

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,17 +2,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out,                            "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -21,17 +21,6 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 require("lazy").setup("plugins")
-
--- Key mappings
-vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("i", "<C-x>", "<Esc>:q<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-x>", ":q<CR>", { noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-X>", ":qa!<CR>", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("n", "D", "dd", { noremap = true, silent = true })
-
-vim.api.nvim_set_keymap("n", "<leader>t", ":FloatermToggle<CR>", { noremap = true })
 
 -- Enable line numbers
 vim.cmd("set number")
@@ -53,3 +42,6 @@ vim.opt.spell = true
 
 -- Set languages for spell checking
 vim.opt.spelllang = { "en_us" }
+
+-- Load keybindings from a separate file.
+require("keybindings")
---
+++ .config/nvim/lua/keybindings.lua
@@ -0,0 +1,18 @@
+-- This file contains only custom keybindings for neovim.
+
+-- Normal mode mappings
+vim.api.nvim_set_keymap("n", "J", ":m +1<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "U", ":m -2<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-x>", ":q<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<C-X>", ":qa!<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "D", "dd", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<leader>t", ":FloatermToggle<CR>", { noremap = true })
+
+-- Insert mode mappings
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("i", "<C-x>", "<Esc>:q<CR>", { noremap = true, silent = true })
+
+-- Visual mode mappings
+vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
