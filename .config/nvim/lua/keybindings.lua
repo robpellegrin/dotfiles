9b4fb64edfc6fd2cea2bdbf3b2425e0b0bcc4149
Rob Pellegrin <>
2024-11-20 11:46:50 -0500

    New keybindings to just to first or last character on current line

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -1,6 +1,6 @@
 -- This file contains only custom keybindings for neovim.
 
--- Normal mode mappings
+--- Normal mode mappings
 vim.api.nvim_set_keymap("n", "J", ":m +1<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "U", ":m -2<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
@@ -8,11 +8,15 @@ vim.api.nvim_set_keymap("n", "<C-x>", ":q<CR>", { noremap = true, silent = true 
 vim.api.nvim_set_keymap("n", "<C-X>", ":qa!<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "D", "dd", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("n", "<leader>t", ":FloatermToggle<CR>", { noremap = true })
+vim.api.nvim_set_keymap("n", "0", "^", { noremap = true })
+vim.api.nvim_set_keymap("n", "-", "g_", { noremap = true })
 
--- Insert mode mappings
+--- Insert mode mappings
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<C-s>", "<Esc>:w<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("i", "<C-x>", "<Esc>:q<CR>", { noremap = true, silent = true })
 
--- Visual mode mappings
+--- Visual mode mappings
 vim.api.nvim_set_keymap("v", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.api.nvim_set_keymap("v", "0", "^", { noremap = true })
+vim.api.nvim_set_keymap("v", "-", "g_", { noremap = true })
