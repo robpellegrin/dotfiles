bf813f08386ddb70143a48b6ce9bf9fc8fc6b32e
Rob Pellegrin <>
2024-11-21 18:17:57 -0500

    Fixed typo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,7 +16,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Set mapleader before lazy.nvim is loaded chickn
+-- Set mapleader before lazy.nvim is loaded.
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
