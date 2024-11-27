9dd5054c2145fea51ac741e2c3a73b6ddff72ee2
Rob Pellegrin <>
2024-11-26 23:03:04 -0500

    Added Python LSP

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,40 +1,44 @@
 -- lsp-config.lua
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "bashls", "clangd" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
-			local lspconfig = require("lspconfig")
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls", "bashls", "clangd", "pylsp" },
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
 
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
 
       lspconfig.rust_analyzer.setup({
         capabilities = capabilities,
       })
 
-			-- Key bindings
-			vim.keymap.set("n", "H", vim.lsp.buf.hover, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+      lspconfig.pylsp.setup({
+        capabilities = capabilities,
+      })
+
+      -- Key bindings
+      vim.keymap.set("n", "H", vim.lsp.buf.hover, {})
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+    end,
+  },
 }
