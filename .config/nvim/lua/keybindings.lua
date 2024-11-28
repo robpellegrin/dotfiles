c8b1f5067b02199eacb403b75d9df6d0203f4b62
Rob Pellegrin <>
2024-11-27 20:03:22 -0500

    New keybindings for F and B

--- .config/nvim/lua/keybindings.lua
+++ .config/nvim/lua/keybindings.lua
@@ -12,6 +12,8 @@ vim.api.nvim_set_keymap("n", "0", "^", { noremap = true })
 vim.api.nvim_set_keymap("n", "-", "g_", { noremap = true })
 vim.api.nvim_set_keymap("n", "j", "k", { noremap = true })
 vim.api.nvim_set_keymap("n", "k", "j", { noremap = true })
+vim.api.nvim_set_keymap("n", "F", "<C-f>", { noremap = true })
+vim.api.nvim_set_keymap("n", "B", "<C-b>", { noremap = true })
 
 --- Insert mode mappings
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = true, silent = true })
