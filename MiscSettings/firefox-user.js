d307feb4334247f897ea8954ccaf06b05034daa4
Rob Pellegrin <>
2024-12-25 19:36:16 -0500

    Added new Firefox settings

---
+++ MiscSettings/firefox-user.js
@@ -0,0 +1,19 @@
+user_pref("media.gmp-widevinecdm.visible", false);
+user_pref("network.protocol-handler.external.mailto", false);
+user_pref("keyword.enabled", false);
+user_pref("browser.toolbars.bookmarks.visibility", "never");
+user_pref("privacy.clearOnShutdown_v2.cookiesAndStorage", true);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+user_pref("privacy.sanitize.sanitizeOnShutdown", true);
+user_pref("privacy.sanitize.pending", [{"id":"shutdown","itemsToClear":["cache","siteSettings","historyFormDataAndDownloads","cookiesAndStorage"],"options":{}}]);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("browser.urlbar.suggest.quicksuggest.sponsored", false);
+user_pref("browser.urlbar.suggest.quicksuggest.nonsponsored", false);
+user_pref("browser.startup.homepage", about:home);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.urlbar.suggest.recentsearches", false);
+user_pref("browser.search.suggest.enabled", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+
