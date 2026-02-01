de9d6e235c46d42deeeead8ef485ec01d50de46f
Rob Pellegrin <>
2026-02-01 16:13:03 -0500

    Rewritten for use with new treesitter API. Old method in place but commented out.

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,27 @@
 -- Treesitter.lua
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function ()
+    local treesitter = require("nvim-treesitter")
+    treesitter.setup({auto_install = true,})
+  end
+ }
 
-    config = function()
-      local configs = require("nvim-treesitter.configs")
-      configs.setup({
-        auto_install = true, -- auto add new languages.
-        sync_install = false,
-        highlight = { enable = true },
-        indent = { enable = true },
-      })
-    end,
-  },
-}
+--return {
+--  {
+--    "nvim-treesitter/nvim-treesitter",
+--    build = ":TSUpdate",
+--
+--    config = function()
+--      local configs = require("nvim-treesitter.configs")
+--      config.setup({
+--        auto_install = true, -- auto add new languages.
+--        sync_install = false,
+--        highlight = { enable = true },
+--        indent = { enable = true },
+--      })
+--    end,
+--  },
+--}
